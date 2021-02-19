6c60c04003f62a543b5a8d339f21e5aa3bb16f4f
Juho BAE <juho.bae@veolia.com>
2021-02-19 14:54:57 +0900

    Add side bar

--- src/components/AppShell.js
+++ src/components/AppShell.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link as RouterLink} from 'react-router-dom';
+import Link from '@material-ui/core/Link';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Drawer from '@material-ui/core/Drawer';
@@ -23,19 +25,33 @@ class AppShell extends React.Component{
         };
     }
     handlerDrawerToggle = () => this.setState({toggle: !this.state.toggle})
+    handlerCloseDrawerToggle = (bToggle) => this.setState({toggle: bToggle})
+
     render(){
         const {classes } = this.props;
         return(
-            <div className={classes.root}>
-                <AppBar position="static">
-                    <IconButton className={classes.menuButton} color="inherit" onClick={this.handlerDrawerToggle}>
-                        <ManuIcon/>
-                    </IconButton>
-                </AppBar>
-                <Drawer open={this.state.toggle}> 
-                    <MenuItem onClick={this.handlerDrawerToggle}>Home</MenuItem>
-                    <MenuItem onClick={this.handlerDrawerToggle}>Home2</MenuItem>
-                </Drawer>
+            <div>
+                <div className={classes.root}>
+                    <AppBar position="static">
+                        <IconButton className={classes.menuButton} color="inherit" onClick={this.handlerDrawerToggle}>
+                            <ManuIcon/>
+                        </IconButton>
+                    </AppBar>
+                    <Drawer open={this.state.toggle}> 
+                        <MenuItem onClick={this.handlerDrawerToggle}>
+                            <Link component={RouterLink} to="/">Home</Link>
+                        </MenuItem>
+                        <MenuItem onClick={this.handlerDrawerToggle}>
+                            <Link component={RouterLink} to="/texts">Texts</Link>
+                        </MenuItem>
+                        <MenuItem onClick={this.handlerDrawerToggle}>
+                            <Link component={RouterLink} to="/words">Words</Link>
+                        </MenuItem>
+                    </Drawer>
+                </div>
+                <div id="content" style={{margin: 'auto', marginTop: '20px'}}>
+                    {React.cloneElement(this.props.children)}
+                </div>
             </div>
         )
     }
